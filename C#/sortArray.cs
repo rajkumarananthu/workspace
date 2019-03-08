c5f98fed4a095a988990e140a6857fa809886e3b
Rajkumar Ananthu <rajkumar.ananthu108@gmail.com>
2019-03-08 12:04:09 +0530

    2nd commit

---
+++ C#/sortArray.cs
@@ -0,0 +1,26 @@
+using System;
+
+class sortArray {
+    /** here if static is must and should, why? I think the reason is that as Main is
+     *  a static method, it doesn't allow a non static method all in it */
+    static void bubbleSort(int []a) {
+        for (int i = 0; i < a.Length; ++i) {
+            for (int j = 0; j < a.Length; ++j) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp; 
+                }
+            }
+        }
+    }
+    static void Main(string []args) {
+        int []arr = {40,10,3,67,13,25,1002,204,367,122};
+        bubbleSort(arr);
+        Console.WriteLine("Sorted Array:");
+        for (int i =0; i < arr.Length; ++i) {
+            Console.Write(arr[i]);
+            Console.Write(" ");
+        }
+    }
+}
---
+++ C#/valueTypes.cs
@@ -0,0 +1,11 @@
+using System;
+
+class valueTypes {
+    static void Main(string []args) {
+        int a = 10;
+        char b = 'a';
+        float c = (float)123.4;
+        Console.WriteLine("Integer: " + a + " Charecter: " + b + " Float: " + c);
+        Console.ReadKey();
+    }
+}
