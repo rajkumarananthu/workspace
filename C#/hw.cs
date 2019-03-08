d79d3616ae9132faea0ae2c6ff8fedc81a8534e0
Rajkumar Ananthu <rajkumar.ananthu108@gmail.com>
2019-03-08 11:29:43 +0530

    Added a new dir for C# practising

---
+++ C#/hw.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HW
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.Read();
+        }
+    }
+}
