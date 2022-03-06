83a30062151c919a38b758ccb4b5c48b3e44f1e7
Rajkumar Ananthu <rajkumar.ananthu108@gmail.com>
2022-03-06 14:57:13 +0530

    Adding the updation of gitmodules in x64dbg project.

--- x64dbg_set_project_settings.sh
+++ x64dbg_set_project_settings.sh
@@ -12,3 +12,5 @@ sed -i "s/v120_xp/$VS_SDK_VER/g" src/gui/Src/Gui/LogView.h
 sed -i "s/v120_xp/$VS_SDK_VER/g" src/launcher/x64dbg_launcher.vcxproj
 sed -i "s/v120_xp/$VS_SDK_VER/g" src/loaddll/loaddll.vcxproj
 sed -i "s/v120_xp/$VS_SDK_VER/g" src/zydis_wrapper/zydis_wrapper.vcxproj
+# Updating the submodules, to clone the modules from x64dbg repo
+sed -i "s/\.\./git@github.com:x64dbg/g" .gitmodules
