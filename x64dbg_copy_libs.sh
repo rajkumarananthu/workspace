1190ef635f3c7a97b2f30a2be3ea99abb23b7671
Rajkumar Ananthu <rajkumar.ananthu108@gmail.com>
2022-04-02 10:47:15 +0530

    Updating copy libs script for x64dbg

--- x64dbg_copy_libs.sh
+++ x64dbg_copy_libs.sh
@@ -2,4 +2,6 @@
 echo "Should be un after building the project in Visual Studio with sln file.\n"
 cp -rf deps/x64/* bin/x64/
 cp -rf deps/x64d/* bin/x64d/
+cp -rf deps/x32/* bin/x32/
+cp -rf deps/x32d/* bin/x32d/
 echo "Now, replace the Qt*.dll with the actual dlls from your Qt directory"
